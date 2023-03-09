ce12f8df9f683f59175326b70abfd1880c36cbec
Ainhoa Prada <apt.code14@gmail.com>
2023-03-09 19:44:59 +0100

    modal created and responsive

---
+++ src/components/modal/index.js
@@ -0,0 +1,37 @@
+import './index.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function ContactModal({body, title}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="flat" onClick={handleShow}>
+        {title}
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Send me an Email!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <div>{body}</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+export default ContactModal;
+
