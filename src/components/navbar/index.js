cf594910cf26d60e9fb1ba2d06ea1adb1d5550e6
Ainhoa Prada <apt.code14@gmail.com>
2023-03-09 19:43:43 +0100

    structure + Navbar and header done

---
+++ src/components/navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './index.css';
+
+function Navbar() {
+    return (
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 kllk">
+      <li className="nav-item">
+        <NavLink 
+          to="/React-Portfolio"
+          end
+          aria-current="page"
+          className={({isActive}) => 
+            isActive ? 'nav-link active' : 'nav-link'} 
+          >Home</NavLink>
+      </li>
+      <li className="nav-item">
+      <NavLink 
+          to="/React-Portfolio/projects"
+          end
+          className={({isActive}) => 
+            isActive ? 'nav-link active' : 'nav-link'} 
+        >Projects</NavLink>
+      </li>
+      <li className="nav-item">
+      <NavLink 
+          to="/React-Portfolio/contact"
+          end
+          className={({isActive}) => 
+            isActive ? 'nav-link active' : 'nav-link'} 
+      >Contact</NavLink>
+      </li>
+    </ul>
+    );
+  }
+
+export default Navbar;
